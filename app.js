8d784b3f055f209d56be7677cdc2bc6a97c529a5
PJSalter <psalter88@googlemail.com>
2022-02-13 16:56:34 +0000

    event listener that picks up the video once mouse over effect

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 /* --------------------- js fetch api will go here -------------------------*/
+
+const voiceAudio = document.getElementById("sheldonVoice");
+
+/*
+function instantNotify() {
+    voiceAudio.play(); // this function will instantly play the voice.
+}
+
+*/
+
+document.body.addEventListener("mousemove", function () {
+    voiceAudio.play();
+})
