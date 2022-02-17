078145cc3c7e8d792e15fbfa1e062cf36e044fa9
PJSalter <psalter88@googlemail.com>
2022-02-17 14:51:37 +0000

    fixed promise all and created new async functions that pass data

--- app.js
+++ app.js
@@ -17,6 +17,12 @@ commenceVoiceActivate.addEventListener("click", function () {
     voiceAudio.style.display = none;
 })
 
+//api key from GHIPY 
+let API_KEY = "yTk65md4VUpLFHxJvS6bqFOcT31mDqFg";
+
+/* Function to generate random number for different APIs */
+const randomNumber = (number) => Math.floor(Math.random() * (number - 1));
+
 //---------------------------------------------------------------//
 // ------------------ the movie database work  ---------------- //
 // -------------------------------------------------------------//
@@ -36,41 +42,27 @@ let searchURL = `${primary_URL}/search/movie?${tvApiKey}`;
 let posterMovie = 'https://image.tmdb.org/t/p/w500';
 
 // picking up the main tag.
-let mainArea = document.querySelector('main')
+let mainArea = document.getElementById('content-display')
 let form = document.querySelector('form')
 let search = document.querySelector("#message-box")
+let button = document.querySelector(".button")
 
 // url I may work with later that picks up sci fi movies
 // /discover/movie?with_genres=878&with_cast=500&sort_by=vote_average.desc
-
-
 
 // creating function get color and passing in vote.
 let getColor = vote => {
     vote >= 8 ? 'green' : vote >= 5 ? 'orange' : 'red';
 }
 
-// a function to pick up all the movies.
-let pickSciFiMovie = url => {
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.results);
-        //create a function to show movies that will pass on this data/
-        presentMovies(data.results);
-    })
-    .catch(error => console.error(error));
-}
-
-//call the function so that it picks up the data in the console.
-pickSciFiMovie(pickUp_URL);
-
 // creating the function to show the movies
 
 let presentMovies = data => {
     // shows in console that there is an array of 20 that we can loop thru.
-    
-    data.forEach(movie => {
+    //console.log(data);
+    let randomFilm = randomNumber(data.length);
+    //data.forEach((movie) => {
+        let movie = data[randomFilm];
         // setting inner html.
         mainArea.innerHTML = ''
     
@@ -93,7 +85,7 @@ let presentMovies = data => {
       </div>
         
         `
-    
+    //console.log(mainArea)
      //      Add overview div for movie descriptions 
      //     <div class="overview">
      //    <h4>${overview}</h4>
@@ -101,11 +93,93 @@ let presentMovies = data => {
     
         // append all the child elements into the main tag.
         mainArea.appendChild(elFilm);
-    })
     }
+
+    // a function to pick up all the movies.
+let pickSciFiMovie = async (url) => {
+    let response = await fetch(url)
+    return response
+    /*
+    .then((response) => {
+        if (!response.ok) {
+          const error = new Error(response.status)
+          throw error
+        }
+        return response.json()
+      })
+    .then(data => {
+        console.log(data.results); // .results
+        //create a function to show movies that will pass on this data/
+        presentMovies(data.results); // .results
+    })
+    .catch(error => console.error(error));
+    */
+}
+
+const scifiProcess = data => {
+    presentMovies(data.results);
+}
+
+//call the function so that it picks up the data in the console.
+//pickSciFiMovie(pickUp_URL);
+
+//eventListener added 
+async function giphysearch() {
+  
+    let url = `http://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&limit=1&q=`;
+    let str = document.getElementById("message-box").value.trim();
+    url = url.concat(str);
+    let response = await fetch(url);
+
+    return response;
+}
+
+const giphyProcess = content => {
+        let fig = document.createElement("figure");  // fig element created 
+        let img = document.createElement("img"); // image element created
+        // let fc = document.createElement("figcaption");
+       
+        console.log(content)
+        img.src = content.data[randomNumber(content.data.length)].images.downsized.url;  // source for image provided 
+        img.alt = content.data[randomNumber(content.data.length)].title; 
+        
+        //img.src = content.data[randomNumber(content.data.length)].images.downsized.url;  // source for image provided 
+        //img.alt = content.data[randomNumber(content.data.length)].title;  // alt title provided in the event of failure to load image
+        fig.appendChild(img);   // image element added to fig
+
+        let output = document.querySelector(".output");  
+        output.innerHTML = '';
+        output.appendChild(fig);
+        //document.querySelector("#message-box").value = "";
+        button.value = "";
+}
+
 
 // Event listener to grab form tag and link to movie search
 form.addEventListener('submit', (e) => {
+    // prevents form from refreshing when user clicks submit button
+    e.preventDefault();
+
+
+    const searchTerm = search.value;
+
+    if(searchTerm) {
+        fetchAsyncData(searchURL+'&query='+searchTerm)
+    } else {
+        fetchAsyncData(pickUp_URL);
+    }
+
+    console.log('yo')
+
+    //fetchAsyncData()
+
+   //giphysearch(e);
+})
+
+
+/*
+
+button.addEventListener('keydown', (e) => {
     // prevents form from refreshing when user clicks submit button
     e.preventDefault();
 
@@ -117,49 +191,68 @@ form.addEventListener('submit', (e) => {
     } else {
         pickSciFiMovie(pickUp_URL);
     }
+
+    console.log(giphysearch(e));
 })
 
+*/
 
 
 
-let API_KEY = "yTk65md4VUpLFHxJvS6bqFOcT31mDqFg"
-//api key from GHIPY 
-const formEl = document.querySelector('form');
-document.addEventListener("DOMContentLoaded", giphysearch);
+   // movies 
+   //let moviePromise = fetch(`https://api.themoviedb.org/3/discover/movies?sort_by=popularity.desc${tvApiKey}`);
 
-//eventListener added 
-function giphysearch() {
-  document.querySelector(".button").addEventListener("click", event => {
-    event.preventDefault(); 
-    //to stop the page reload
+   // giphy
+   //let githyPromise = fetch(`http://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&limit=1&q=`);
 
-    let url = `http://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&limit=1&q=`;
-    let str = document.getElementById("message-box").value.trim();
-    url = url.concat(str);
-//field input will be added to the end of the url using concat method 
+  // Observing with timing functions
+/*
+  const PromiseTimeOne = new Promise((moviePromise) => {
+      setTimeout(() =>{
+          const newEstimate = Math.floor(Math.random() * 20);
+          moviePromise(newEstimate)
+      }, 5000);
+  })
 
-  //fetch request send using the url  
-    fetch(url) 
-      .then(response =>  {
-        if(!response.ok) throw new error(response.status);
-        return response.json();
-      })
-      .then(content => {     // data from response 
+  const PromiseTimeTwo = new Promise((githyPromise) => {
+    setTimeout(() =>{
+        const newEstimate = Math.floor(Math.random() * 20);
+        githyPromise(newEstimate)
+    }, 8000);
+})
+
+*/
+
+const fetchAsyncData = async (search) => {
+const response = await Promise.all([giphysearch(),  pickSciFiMovie(search)])
+console.log(response)
+const responseData = await Promise.all(response.map(el => el.json()))
+console.log(responseData)
+
+giphyProcess(responseData[0])
+scifiProcess(responseData[1])
 
 
-        let fig = document.createElement("figure");  // fig element created 
-        let img = document.createElement("img");   // image element created
-        // let fc = document.createElement("figcaption");
-        img.src = content.data[0].images.downsized.url;  // source for image provided 
-        img.alt = content.data[0].title;  // alt title provided in the event of failure to load image
-        fig.appendChild(img);   // image element added to fig
-
-        let output = document.querySelector(".output");  
-        output.appendChild(fig);
-        document.querySelector("#message-box").value = "";
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  });
-} 
+//Promise.all([moviePromise, githyPromise])
+/*
+     .then((apisResponsesArray) => {
+      // Return another promise as an array with the parsed content of the previous response.
+      return Promise.all(
+        apisResponsesArray.map((apiResponse) => apiResponse.json()),
+      )
+    })
+    .then((parsedReponsesArray) => {
+        console.log(parsedReponsesArray)
+      // Destructure the parsed reponses array into separate variables.
+      const [movieResponse, giphyResponse] = parsedReponsesArray
+       
+      // Call functions to display content of the separate responses of the APIs.
+      //giphysearch(giphyResponse);
+      //giphysearch(giphyResponse);
+      //presentMovies(movieResponse)
+      //pickSciFiMovie(movieResponse)
+      //pickSciFiMovie(movieResponse);
+    })
+    .catch((error) => console.log(error))
+    */
+}
